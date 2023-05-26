05e0d5242853cebe2d2be182eb8940e169da7328
davidjoohnson <you@example.com>
2023-05-26 13:13:09 +0200

    busqueda + fondo difuminado + animacion titulos

--- components/script.js
+++ components/script.js
@@ -1,18 +1,1 @@
 
-/*
-window.addEventListener("scroll", function () {
-    var hero = document.querySelector(".hero");
-    var heroContent = document.querySelector(".hero-content");
-
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTop > 100) {
-        hero.classList.add("unblur");
-        heroContent.classList.add("hidden");
-    } else {
-        hero.classList.remove("unblur");
-        heroContent.classList.remove("hidden");
-    }
-});
-
-*/
