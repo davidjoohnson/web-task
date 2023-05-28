5b1c336e4a7ea58d360625571bcef2a1ebe4e76b
davidjoohnson <you@example.com>
2023-05-28 17:59:15 +0200

    reloj-final

--- components/script.js
+++ components/script.js
@@ -14,3 +14,23 @@ function formatoDosDigitos(valor) {
 }
 
 setInterval(actualizarReloj, 1000);
+
+// Cambiar color al hacer clic en el botón
+var botonColor = document.getElementById("cambiarColor");
+botonColor.addEventListener("click", function () {
+    var letrasReloj = document.querySelectorAll(".reloj span");
+    letrasReloj.forEach(function (letra) {
+        var color = generarColorAleatorio();
+        letra.style.color = color;
+    });
+});
+
+function generarColorAleatorio() {
+    var letras = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letras[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
